844cb47678be28bb3230691eb1826762b0ce8758
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2025-04-16 12:22:38 +0100

    fix: no caching for webpack on dev server

--- next.config.js
+++ next.config.js
@@ -13,6 +13,12 @@ const nextConfig = {
     ],
   },
   swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
