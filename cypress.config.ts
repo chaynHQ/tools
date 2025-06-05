578a56cf782bbf1f502cf3f66dcd8b17229c8070
Anna Hughes <annaraehughes@live.co.uk>
2025-06-05 16:25:40 +0100

    Fix cypress action (#58)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     video: false,
     screenshotOnRunFailure: false,
-    experimentalWebKitSupport: true
+    experimentalWebKitSupport: true,
   },
 });
